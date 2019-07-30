e2e0fd157919804aa779ae7b8eaa61c867f479d8
Marta <marta.jastrzabek@gmail.com>
2019-07-30 21:23:29 +0200

    change in sumDigits method

--- src/Sum.java
+++ src/Sum.java
@@ -2,11 +2,9 @@ public class Sum {
     public static int sumDigits(int number){
         String num = Integer.toString(number);
         int sum=0;
-        int[] digits = new int[num.length()];
-        for (int i = 0; i<digits.length; i++){
-            digits[i] = number%10;
+        for (int i = 0; i<num.length(); i++){
+            sum+= number%10;
             number/=10;
-            sum+=digits[i];
         }
         return sum;
     }
